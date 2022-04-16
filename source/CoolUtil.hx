bbafbe5399f79167c85010298d4848d8b2010b34
Theoyeah <97792861+Theoyeah@users.noreply.github.com>
2022-04-16 14:42:52 +0200

    Deleted fucked for now, until i start adding charts

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -19,8 +19,7 @@ class CoolUtil
 	public static var defaultDifficulties:Array<String> = [
 		'Easy',
 		'Normal',
-		'Hard',
-		'FUCKED'
+		'Hard'
 	];
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
