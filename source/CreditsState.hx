648ea15ba26e742333aa14e653bf9b3162468d21
Theoyeah <theodelalot@hotmail.com>
2022-04-16 13:16:18 +0200

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,6 +81,9 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+			['Theoyeah Engine Team'],
+			['Theoyeah',		'theoyeah credit',		'Creator of Theoyeah Engine',							'',	'009BF4'],
+			[''],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',							'https://twitter.com/Shadow_Mario_',	'444444'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',						'https://twitter.com/RiverOaken',		'C30085'],
