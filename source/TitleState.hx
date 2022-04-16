58bc19a777d62b02fdf8420a55c9ea70b0fe3786
Theoyeah <theodelalot@hotmail.com>
2022-04-16 15:50:07 +0200

    Just smth

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -439,7 +439,7 @@ class TitleState extends MusicBeatState
 			{
 				if(titleText != null) titleText.animation.play('press');
 
-				FlxG.camera.flash(FlxColor.ORANGE, 1);
+				FlxG.camera.flash(FlxColor.WHITE, 1);
 				FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
 
 				transitioning = true;
