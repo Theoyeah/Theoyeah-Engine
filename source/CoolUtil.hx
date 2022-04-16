d74a4b3962715fd4ec60fae87e7d66b6767aa7e5
Theoyeah <theodelalot@hotmail.com>
2022-04-16 13:59:24 +0200

    Fucked is coming..

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -19,7 +19,8 @@ class CoolUtil
 	public static var defaultDifficulties:Array<String> = [
 		'Easy',
 		'Normal',
-		'Hard'
+		'Hard',
+		'FUCKED'
 	];
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
