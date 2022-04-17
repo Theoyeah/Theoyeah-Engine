29cb7ccf5cf63b60cbedddcaa7a84bb5f3b0a1ba
Theoyeah <97792861+Theoyeah@users.noreply.github.com>
2022-04-17 17:09:43 +0200

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineVersion:String = '1.0'; //This is also used for Discord RPC
+	public static var psychEngineVersion:String = '2.0'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
