3b92a36041e314dd76fefd954a14620c676f3376
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-04-21 10:24:02 -0500

    Guess not…

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1722,7 +1722,7 @@ class FunkinLua {
 			FlxG.sound.music.fadeOut(duration, toValue);
 			luaTrace('musicFadeOut is deprecated! Use soundFadeOut instead.', false, true);
 		});
-		Lua_helper.add_callback(lua, "chromaticEffect", function(camera:String, chromeOffset:Float = 0.005) {
+		/*Lua_helper.add_callback(lua, "chromaticEffect", function(camera:String, chromeOffset:Float = 0.005) {
 			
 			PlayState.instance.addShaderToCamera(camera, new ChromaticAberrationEffect(chromeOffset));
 			
@@ -1789,7 +1789,7 @@ class FunkinLua {
 		});
 		Lua_helper.add_callback(lua, "killShaders", function(camera:String) {
 			PlayState.instance.clearShaderFromCamera(camera);
-		});
+		});*/
 					
 		call('onCreate', []);
 		#end
