d819b1dc7e2dc67c1f8d9a3572dc4ec4af6c73f7
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-05-04 10:58:23 -0500

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -62,6 +62,7 @@ class ChartingState extends MusicBeatState
 		'Hey!',
 		'Hurt Note',
 		'Instakill Note',
+		'Crash Note', //so we can actually test it
 		//'Poisoned Note', Not working for now
 		'GF Sing',
 		'No Animation'
