708da328734635a35e2c485c1716a3f7e6232adf
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-05 13:33:26 +0200

    Freaking error fixed

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -62,7 +62,7 @@ class TitleState extends MusicBeatState
 	var credTextShit:Alphabet;
 	var textGroup:FlxGroup;
 	var ngSpr:FlxSprite;
-	var ty:FlxSprite;
+	var tySpr:FlxSprite;
 
 	var curWacky:Array<String> = [];
 
@@ -378,12 +378,12 @@ class TitleState extends MusicBeatState
 		ngSpr.screenCenter(X);
 		ngSpr.antialiasing = ClientPrefs.globalAntialiasing;
 		
-		ty = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('theoyeah_logo'));
-		add(ty);
-		ty.visible = false;
-		ty.setGraphicSize(Std.int(125 * 0.74); //i dont know how this works, edit it later theoyeah to correct the image and all that
-		ty.updateHitBox();
-		ty.screenCenter(X);
+		tySpr = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('theoyeah_logo'));
+		add(tySpr);
+		tySpr.visible = false;
+		tySpr.setGraphicSize(Std.int(125 * 0.74)); //i dont know how this works, edit it later theoyeah to correct the image and all that
+		tySpr.updateHitBox();
+		tySpr.screenCenter(X);
 		
 		
 
