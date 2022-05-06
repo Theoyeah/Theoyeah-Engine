de39b4191a9c7d82b39747360874f73a4be0d02b
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-05-06 14:01:36 -0500

    Winning Icons Option

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -40,6 +40,13 @@ class VisualsUISubState extends BaseOptionsMenu
 			'bool',
 			true);
 		addOption(option);
+		
+		var option:Option = new Option('Winning Icons',
+			'If checked, enables extra icon frames',
+			'winningIcon',
+			'bool',
+			true);
+		addOption(option);
 
 		var option:Option = new Option('Kade Engine Score Text',
 			"If checked, the text below the health bar will change to Kade Engine (except the style).",
