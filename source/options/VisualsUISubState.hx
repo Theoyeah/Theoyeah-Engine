cdeae8df619efe88bb2b16eeb8b85639ff6e71bf
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-05-06 14:12:11 -0500

    meee wen gwammr

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -85,7 +85,7 @@ class VisualsUISubState extends BaseOptionsMenu
 		addOption(option);
 		
 		var option:Option = new Option('Icon Bounce:',
-			'How should you icons bounce?',
+			'How should your icons bounce?',
 			'iconBounce',
 			'string',
 			'Default',
