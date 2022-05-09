29f58434e5d2c337db94eec592668d15d9415341
Theoyeah <theodelalot@hotmail.com>
2022-05-09 20:52:07 +0200

    thing i forgor

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -113,7 +113,7 @@ class NoteOffsetState extends MusicBeatState
 
 		combosprshit = new FlxSprite().loadGraphic(Paths.image('combo'));
 		combosprshit.cameras = [camHUD];
-		combosprshit.setGraphicSize(Std.int(rating.width * 0.7));
+		combosprshit.setGraphicSize(Std.int(combosprshit.width * 0.7));
 		combosprshit.updateHitbox();
 		combosprshit.antialiasing = ClientPrefs.globalAntialiasing;
 		add(combosprshit);
