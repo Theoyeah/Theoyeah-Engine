bd9b1b397644ac2db465fd82f0d2465f290f54e7
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-11 10:31:51 +0200

    Update FlxThings.hx

--- source/FlxThings.hx
+++ source/FlxThings.hx
@@ -4,6 +4,9 @@ import flixel.FlxSprite;
 
 using StringTools;
 
+/*
+stoled from HaxeFlixel Docs!!
+*/
 class NewFlxSprite extends FlxSprite
 {
 	public function setGraphic(sprite:FlxSprite, x:Float, y:Float) {
@@ -25,3 +28,27 @@ class NewFlxSprite extends FlxSprite
 
 	
 }
+
+class MouseThings {
+	function mousePressed(returnFalse:Bool) {
+		if (FlxG.mouse.pressed) {
+			return true;
+			// The left mouse button is currently pressed
+		} else if (returnFalse) {
+			return false;
+		}
+	}
+
+	function mouseJustPressed(returnFalse:Bool) {
+		if (FlxG.mouse.justPressed) {
+			// The left mouse button has just been pressed
+			return true;
+		} else if(returnFalse) {
+			return false;
+		}
+	}
+
+    if (FlxG.mouse.justReleased)
+    {
+        // The left mouse button has just been released
+    }
