9c848c09645adbff33f841030fa28f54e065b89a
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-13 08:55:03 +0200

    Update FlxThings.hx

--- source/FlxThings.hx
+++ source/FlxThings.hx
@@ -117,6 +117,27 @@ class FlxThings
 	
 	}
 
+	class Figures //i dont know english :'<
+	{
+		class NewRectangle extends Rectangle
+		{
+			//to create a rectangle use this new(x:Float = 0, y:Float = 0, width:Float = 0, height:Float = 0)
+			public function getWidthOrHeight(figure:Rectangle, returnWidth:Bool, ?returnHeight:Bool) {
+				var heightt:Bool = if(returnWidth) false else true;
+				var widthh:Bool = if(returnHeight) false else true;
+				var theTwoOfUs:Bool if(returnHeight && returnWidth) true else false;
+				if(!theTwoOfUs) {
+					if(widthh) {
+						return figure.width;
+					} else if(heightt) {
+						return figure.height;
+					}
+				}
+				
+			}
+		}
+	}
+	
 	class MouseThings extends FlxMouse
 	{
 		public function mousePressed(?returnFalse:Bool = false) {
