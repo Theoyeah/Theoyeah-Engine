8f0ad262ce4a077a22663a35d78d13c1c655b914
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-15 11:21:58 +0200

    Now we don’t have necessary to write unknown to the icon thing

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -138,7 +138,7 @@ class CreditsState extends MusicBeatState
 					Paths.currentModDirectory = creditsStuff[i][5];
 				}
 
-				var icon:AttachedSprite = new AttachedSprite('credits/' + creditsStuff[i][1]);
+				var icon:AttachedSprite = if(creditsStuff[i][1] == '') new AttachedSprite('credits/unknown') else new AttachedSprite('credits/' + creditsStuff[i][1]);
 				icon.xAdd = optionText.width + 10;
 				icon.sprTracker = optionText;
 	
