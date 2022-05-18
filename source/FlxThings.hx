09cb38ba4ad9f98abbf8a5e1a3ede00647e8bbbf
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-18 16:11:07 +0200

    Better getArea

--- source/FlxThings.hx
+++ source/FlxThings.hx
@@ -149,10 +149,11 @@ class FlxThings
 				return Math.PI * 2 * radius;
 			}
 			public function getLengthRadius(radius:Float, numberOfDegrees:Float):Float {
-				return (Math.PI*2*radius*numberOfDegrees)/360;
+				return (Math.PI * 2* radius * numberOfDegrees) / 360;
 			}
-			public function getArea(radius:Float):Float {
-				return Math.PI * Math.pow(radius, 2);
+			public function getArea(value:Float, ?radius:Bool = true):Float {
+				var v:Float = if(!radius) value / 2 else value;
+				return Math.PI * Math.pow(v, 2);
 			}
 			
 		}
