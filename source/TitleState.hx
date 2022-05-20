06f56052c48ec1bd66249d37846e4d33835e165c
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-20 18:02:39 +0200

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -640,7 +640,6 @@ class TitleState extends MusicBeatState
 			switch (sickBeats)
 			{
 				case 1: 
-					FlxG.sound.music.stop();
 					FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 					FlxG.sound.music.fadeIn(4, 0, 0.7);
 				case 2:
