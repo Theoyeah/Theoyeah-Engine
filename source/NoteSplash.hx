ae63501d8e24f5521d1a2a2ffc8fab46575a07f7
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-22 19:00:04 +0200

    Fixed (thanks @BeastlyGhost for the offsets)

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -65,11 +65,11 @@ class NoteSplash extends FlxSprite
 		colorSwap.hue = hueColor;
 		colorSwap.saturation = satColor;
 		colorSwap.brightness = brtColor;
-		/*if(texture == 'og_noteSplashes') {
+		if(texture == 'og_noteSplashes') {
 			offset.set(Std.int(0.3 * width), Std.int(0.3 * height));
-		} else {*/
+		} else {
 			offset.set(10, 10);
-		//}
+		}
 		var animNum:Int = FlxG.random.int(1, 2);
 		animation.play('note' + note + '-' + animNum, true);
 		if(animation.curAnim != null) animation.curAnim.frameRate = 24 + FlxG.random.int(-2, 2);
