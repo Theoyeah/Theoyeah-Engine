0d086fe22992b0e828193a404040b5a922f9e888
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-22 11:11:17 +0200

    AM I STUPID!?!

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -62,7 +62,7 @@ class NoteSplash extends FlxSprite
 	}
 
 	function loadAnims(skin:String) {
-		frames = Paths.getNoteSplashes(skin);
+		frames = Paths.getSparrowAtlas('noteSplashes/' + skin);
 		for (i in 1...3) {
 			animation.addByPrefix("note1-" + i, "note splash blue " + i, 24, false);
 			animation.addByPrefix("note2-" + i, "note splash green " + i, 24, false);
