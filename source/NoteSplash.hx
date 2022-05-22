10038142462bd0e1bca8ec1d9a5af235cf723e2b
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-22 17:42:19 +0200

    Better, and added @BeastlyGhost splashes

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -20,10 +20,22 @@ class NoteSplash extends FlxSprite
 			case 'pink': hola = 'pink';
 			case 'idk': hola = 'idk';
 			case 'original': hola = 'og';
+			case 'beastlyghost': hola = 'beast';
 			default: hola = 'noteSplashes';
 		}
-		return hola.toLowerCase() + '_noteSplashes';
+		if(hola != 'noteSplashes') {
+			return hola.toLowerCase() + '_noteSplashes';
+		} else {
+			return hola;
+		}
 	}
+
+	/**
+	 * Generates a noteSplash
+	 * @param x x
+	 * @param y y
+	 * @param note what note is (in numbers)
+	 */
 	public function new(x:Float = 0, y:Float = 0, ?note:Int = 0) {
 		super(x, y);
 
@@ -54,8 +66,11 @@ class NoteSplash extends FlxSprite
 		colorSwap.hue = hueColor;
 		colorSwap.saturation = satColor;
 		colorSwap.brightness = brtColor;
-		offset.set(10, 10);
-
+		if(texture == 'beast_noteSplashes') {
+			offset.set(Std.int(0.3 * width), Std.int(0.3 * height));
+		} else {
+			offset.set(10, 10);
+		}
 		var animNum:Int = FlxG.random.int(1, 2);
 		animation.play('note' + note + '-' + animNum, true);
 		if(animation.curAnim != null) animation.curAnim.frameRate = 24 + FlxG.random.int(-2, 2);
