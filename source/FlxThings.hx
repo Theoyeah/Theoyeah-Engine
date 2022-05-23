c2d6ca3e41ca754b166f0a7a86c968824fabed63
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-23 13:05:49 +0200

    Update FlxThings.hx

--- source/FlxThings.hx
+++ source/FlxThings.hx
@@ -82,7 +82,7 @@ class FlxThings
 {
 	var nothing:Dynamic;
 	
-	class MoreArray /*extends Array *///wtf i dont know
+	abstract class MoreArray /*extends Array *///wtf i dont know
 	{
 		public function deleteItemsExcept(array:Array, ?except:Int = 1):Void { //can someone please prove it for me?
 			for (i in 0...array.length-except) {
