33b90625ea12eb4922b8eb88ea87ceea898152e7
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-23 13:25:48 +0200

    Update circumference.hx

--- source/FlxThings/figures/circumference.hx
+++ source/FlxThings/figures/circumference.hx
@@ -1,3 +1,53 @@
+package flxthings.figures;
+
+//HaxeFlixel API Packages
+import flash.*;
+import flixel.*;
+import haxe.*;
+import hscript.*;
+import lime.*;
+import nape.*;
+import openfl.*;
+import spinehaxe.*;
+
+
+//Game States
+import Shaders;
+import PlayState;
+import Character;
+import HealthIcon;
+import BGSprite;
+import Controls;
+import CoolUtil;
+import CreditsState;
+import ClientPrefs;
+import Discord;
+import GameOverSubState;
+import InputFormatter;
+import Main;
+import MainMenuState;
+import MusicBeatState;
+import MusicBeatSubState;
+import Note;
+import StrumNote;
+import NoteSplash;
+import Paths;
+import Song;
+import StageData;
+import WeekData;
+import animateatlas.AtlasFrameMaker;
+import animateatlas.JSONData;
+import animateatlas.JSONData2020;
+import animateatlas.HelperEnums;
+import animateatlas.Main;
+import animateatlas.displayobject.SpriteAnimationLibrary;
+import animateatlas.displayobject.SpriteMovieClip;
+import animateatlas.displayobject.SpriteSymbol;
+import animateatlas.tilecontainer.TileAnimationLibrary;
+import animateatlas.tilecontainer.TileContainerMovieClip;
+import animateatlas.tilecontainer.TileContainerSymbol;
+
+using StringTools;
 
 class Circumference
 {
