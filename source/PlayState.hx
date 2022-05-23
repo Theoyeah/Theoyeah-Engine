75f993b5e1cc02d0096b9d913bb57a8a1d54d9b9
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-23 09:01:12 +0200

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -917,7 +917,7 @@ class PlayState extends MusicBeatState
 
 		var file:String = Paths.txt(songName + '/' + songName + 'Dialogue'); //Checks for vanilla/Senpai dialogue
 		if (OpenFlAssets.exists(file)) {
-			dialogue = CoolUtil.coolTextFile(file);
+			dialogue = CoolUtil.coolTextFile(file + langSuffix);
 		}
 		var doof:DialogueBox = new DialogueBox(false, dialogue);
 		// doof.x += 70;
