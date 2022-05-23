324b878a137c3ac1f404ca0a48cbce965835c622
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-23 13:46:58 +0200

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -35,6 +35,7 @@ import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 import lime.app.Application;
 import openfl.Assets;
+import flxthings.FlxThings;
 
 using StringTools;
 typedef TitleData =
