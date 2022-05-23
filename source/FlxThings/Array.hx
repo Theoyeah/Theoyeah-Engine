37f939e3bd4c80562d4a0a38b415bbc5d59e037f
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-23 13:15:55 +0200

    Update Array.hx

--- source/FlxThings/Array.hx
+++ source/FlxThings/Array.hx
@@ -1,11 +1,60 @@
+package flxthings.array;
+
+//HaxeFlixel API Packages
+import flash.*;
+import flixel.*;
+import haxe.*;
+import hscript.*;
+import lime.*;
+import nape.*;
+import openfl.*;
+import spinehaxe.*;
 
 
+//Game States
+import Shaders;
+import PlayState;
+import Character;
+import HealthIcon;
+import BGSprite;
+import Controls;
+import CoolUtil;
+import CreditsState;
+import ClientPrefs;
+import Discord;
+import GameOverSubState;
+import InputFormatter;
+import Main;
+import MainMenuState;
+import MusicBeatState;
+import MusicBeatSubState;
+import Note;
+import StrumNote;
+import NoteSplash;
+import Paths;
+import Song;
+import StageData;
+import WeekData;
+import animateatlas.AtlasFrameMaker;
+import animateatlas.JSONData;
+import animateatlas.JSONData2020;
+import animateatlas.HelperEnums;
+import animateatlas.Main;
+import animateatlas.displayobject.SpriteAnimationLibrary;
+import animateatlas.displayobject.SpriteMovieClip;
+import animateatlas.displayobject.SpriteSymbol;
+import animateatlas.tilecontainer.TileAnimationLibrary;
+import animateatlas.tilecontainer.TileContainerMovieClip;
+import animateatlas.tilecontainer.TileContainerSymbol;
 
-class MoreArray /*extends Array *///wtf i dont know
-	{
-		public function deleteItemsExcept(array:Array, ?except:Int = 1):Void { //can someone please prove it for me?
-			for (i in 0...array.length-except) {
-				array.pop();
-			}
+using StringTools;
+
+
+class MoreArray extends FlxThings
+{
+	public function deleteItemsExcept(array:Array, except:Int = 1):Void { //can someone please prove it for me?
+		for (i in 0...array.length-except) {
+			array.pop();
 		}
 	}
+}
