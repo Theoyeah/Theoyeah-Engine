fbea08c306d7d61128c27839ddc0699fffa4d7fd
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-24 11:09:05 +0200

    FREAK

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -101,7 +101,7 @@ class PauseSubState extends MusicBeatSubstate
 				bot = 'Toggle Botplay';
 				bac = 'BACK';
 		}
-		menuItemsOG:Array<String> = [resumeL, restartL, changeL, optionsL, exitL];
+		menuItemsOG = [resumeL, restartL, changeL, optionsL, exitL];
 
 		if(CoolUtil.difficulties.length < 2 || CoolUtil.defaultDifficulties.length < 2) {
 			menuItemsOG.remove(changeL); //No need to change difficulty if there is only one!
