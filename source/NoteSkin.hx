84e58cdfe151c3ef5b7b5575b1ecd4ca4f0d9e4e
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-25 19:49:28 +0200

    Update NoteSkin.hx

--- source/NoteSkin.hx
+++ source/NoteSkin.hx
@@ -3,11 +3,11 @@ package;
 
 using StringTools;
 
-class NoteSkins
+class NoteSkin extends Note extends StrumNote extends NoteSplash
 {
-	public function noteSkin():String {
+	public static function noteSkin():String {
 		var skin:String = PlayState.SONG.arrowSkin;
-		if((skin == 'noteSplashes' || skin == '') && ClientPrefs.noteskin != 'Arrows') {
+		if((skin == null || skin == '' || skin.length < 1) && ClientPrefs.noteskin != 'Arrows') {
 			switch(ClientPrefs.noteskin.toLowerCase()) {
 				case 'circles':
 					skin = '-Circles';
@@ -17,7 +17,7 @@ class NoteSkins
 		}
 		return skin;
 	}
-	public function noteSkinFile(folder:Bool = true) {
+	public static function noteSkinFile(folder:Bool = true) {
 		if(folder) {
 			if(noteSkin() != PlayState.SONG.arrowSkin) {
 				return 'noteSkins/NOTE_assets' + noteSkin();
