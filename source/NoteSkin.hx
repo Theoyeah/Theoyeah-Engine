c804b5bf2b01e87a34fb1b3c1ccc4dbe625970e1
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-25 19:16:29 +0200

    Update NoteSkin.hx

--- source/NoteSkin.hx
+++ source/NoteSkin.hx
@@ -1,1 +1,29 @@
+package;
 
+
+using StringTools;
+
+class NoteSkins
+{
+	public function noteSkin():String {
+		var skin:String = PlayState.SONG.arrowSkin;
+		if((skin == 'noteSplashes' || skin == '') && ClientPrefs.noteskin != 'Arrows') {
+			switch(ClientPrefs.noteskin.toLowerCase()) {
+				case 'circles':
+					skin = '-Circles';
+				default:
+					skin = '';
+			}
+		}
+		return skin;
+	}
+	public function noteSkinFile(folder:Bool = true) {
+		if(folder) {
+			if(noteSkin() != PlayState.SONG.arrowSkin) {
+				return 'noteSkins/NOTE_assets' + noteSkin();
+			}
+		} else {
+			return 'NOTE_assets' + noteSkin();
+		}
+	}
+}
