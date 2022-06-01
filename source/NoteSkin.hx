e9eae9a2593497a77ebbafded677c7a6a95d9aeb
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-06-01 13:52:33 +0200

    Update NoteSkin.hx

--- source/NoteSkin.hx
+++ source/NoteSkin.hx
@@ -5,7 +5,7 @@ using StringTools;
 
 class NoteSkin
 {
-	public static function noteSkin():String {
+	inline public static function noteSkin():String {
 		var skin:String = PlayState.SONG.arrowSkin;
 		if((skin == null || skin == '') && ClientPrefs.noteskin != 'Arrows') {
 			switch(ClientPrefs.noteskin.toLowerCase()) {
@@ -22,15 +22,15 @@ class NoteSkin
 
 	public static var noteSkinPlay:Bool = if((noteSkin().length > 0 || noteSkin() != null) && noteSkin() != PlayState.SONG.arrowSkin) true else false;
 
-	public static function noteSkinFile(folder:Bool = true) {
+	inline public static function noteSkinFile(folder:Bool = true) {
 		if(folder) {
-			if(noteSkinPlay) {
+			if(reloadNoteSkinPlay()) {
 				return 'noteSkins/NOTE_assets' + noteSkin();
 			} else {
 				return 'noteSkins/NOTE_assets';
 			}
 		} else {
-			if(noteSkinPlay) {
+			if(reloadNoteSkinPlay()) {
 				return 'NOTE_assets' + noteSkin();
 			} else {
 				return 'NOTE_assets';
@@ -38,10 +38,12 @@ class NoteSkin
 		}
 	}
 	
+	inline public static
 	
 	
-	public static function reloadNoteSkinPlay() {
+	inline public static function reloadNoteSkinPlay() {
 		noteSkinPlay = if((noteSkin().length > 0 || noteSkin() != null) && noteSkin() != PlayState.SONG.arrowSkin) true else false;
+		return noteSkinPlay;
 	}
 
 }
