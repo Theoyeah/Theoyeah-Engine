2a808ab78620f4ec087854995e3c60cb8a0ab430
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-06-06 13:21:03 +0200

    Update Note.hx

--- source/Note.hx
+++ source/Note.hx
@@ -31,6 +31,12 @@ class Note extends FlxSprite
 	public var hitByOpponent:Bool = false;
 	public var noteWasHit:Bool = false;
 	public var prevNote:Note;
+	public var nextNote:Note;
+
+	public var spawned:Bool = false;
+
+	public var tail:Array<Note> = []; // for sustains
+	public var parent:Note;
 
 	public var sustainLength:Float = 0;
 	public var isSustainNote:Bool = false;
@@ -239,6 +245,9 @@ class Note extends FlxSprite
 		}
 
 		// trace(prevNote);
+
+		if(prevNote!=null)
+			prevNote.nextNote = this;
 
 		if (isSustainNote && prevNote != null)
 		{
