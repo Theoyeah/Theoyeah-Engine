e16c0e0dd7bd32538cfe0b038b7c74742206370d
Theoyeah <theodelalot@hotmail.com>
2022-06-10 15:36:13 +0200

    djkdbkjsbdh

--- source/Paths.hx
+++ source/Paths.hx
@@ -13,7 +13,7 @@ import openfl.utils.AssetType;
 import openfl.utils.Assets as OpenFlAssets;
 import lime.utils.Assets;
 import flixel.FlxSprite;
-#if MODS_ALLOWED
+#if sys
 import sys.io.File;
 import sys.FileSystem;
 #end
