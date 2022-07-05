3fdac60f8f781c610aa014dba19316daffde9dd7
Theoyeah <theodelalot@hotmail.com>
2022-07-05 15:41:05 +0200

    Update VisualsUISubState.hx

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -150,14 +150,15 @@ class VisualsUISubState extends BaseOptionsMenu
 		option.onChange = onChangeFPSCounter;
 		#end
 
-		#if CHECK_FOR_UPDATES
+// maybe when 4.0 will be released
+	/*	#if CHECK_FOR_UPDATES
 		var option:Option = new Option('Check for Updates',
 			'On Release builds, turn this on to check for updates when you start the game\nNOT RECOMMENDED.',
 			'checkForUpdates',
 			'bool',
 			true);
 		addOption(option);
-		#end
+		#end */
 
 		super();
 	}
