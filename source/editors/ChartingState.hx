308d6a3805b39095bb82bba78533885aee64dab9
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-08-31 12:32:50 +0200

    This doesn’t work!? Uh?!

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -1730,7 +1730,7 @@ class ChartingState extends MusicBeatState
 
 
 			if (FlxG.keys.justPressed.BACKSPACE) {
-				PlayState.instance.chartingMode = false;
+				//PlayState.instance.chartingMode = false;
 				//if(onMasterEditor) {
 					MusicBeatState.switchState(new editors.MasterEditorMenu());
 					FlxG.sound.playMusic(Paths.music('freakyMenu'));
