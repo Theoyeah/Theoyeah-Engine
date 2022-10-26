279c00a4961f85c592a55d899274cd20b22f37c9
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-10-26 10:29:34 +0200

    Update OutdatedState.hx

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -42,7 +42,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/Wither362/Theoyeah-Engine-DaveAndBambi/releases");
+				CoolUtil.browserLoad("https://github.com/Theoyeah/Theoyeah-Engine/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
